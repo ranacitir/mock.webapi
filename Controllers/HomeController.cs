3da32411e9d493bf45679d232f639c566c659eb0
HP <HP@DESKTOP-CF2IH6U>
2025-10-01 21:31:19 +0300

    Proje dosyası ekle.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Mock.Services;
+using System.Text.Json;
+
+namespace Mock.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        // GET: ApiController
+        [HttpGet]
+        public async Task<IActionResult> Get([FromServices] GoldPriceService goldPriceService)
+        {
+            string fileName = Path.Combine(Directory.GetCurrentDirectory(), "products.json");
+            string jsonString = System.IO.File.ReadAllText(fileName);
+
+            List<Product> products = JsonSerializer.Deserialize<List<Product>>(jsonString)!;
+
+            double goldPrice = await goldPriceService.GetGoldPriceAsync();
+
+            foreach (var product in products)
+            {
+                product.Price = (product.PopularityScore + 1) * product.Weight * goldPrice;
+            }
+
+            return Ok(products);
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok(new { Message = $"You requested item with ID: {id}" });
+        }
+    }
+}
---
+++ Services/GoldPriceServices.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json;
+
+namespace Mock.Services
+{
+    public class GoldPriceService
+    {
+        private readonly HttpClient _httpClient;
+
+        public GoldPriceService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<double> GetGoldPriceAsync()
+        {
+            // Örnek: GoldAPI.io kullanımı
+            _httpClient.DefaultRequestHeaders.Add("x-access-token", "goldapi-fwpichsmg8bfg2t-io");
+
+            var response = await _httpClient.GetAsync("https://www.goldapi.io/api/XAU/USD");
+            response.EnsureSuccessStatusCode();
+
+            var json = await response.Content.ReadAsStringAsync();
+            using var doc = JsonDocument.Parse(json);
+
+            // GoldAPI JSON response'unda "price" alanı gram/USD fiyatını verir
+            double price = doc.RootElement.GetProperty("price").GetDouble();
+            return price;
+        }
+    }
+}
